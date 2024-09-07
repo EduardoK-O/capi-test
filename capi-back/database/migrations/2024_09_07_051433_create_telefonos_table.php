08f7500e49291e707a6e31232de3f8479cb2e602
EduardoK-O <e.kuojeda@gmail.com>
2024-09-06 23:54:33 -0600

    Creación de la Migración del modelo Telefono

---
+++ capi-back/database/migrations/2024_09_07_051433_create_telefonos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTelefonosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefonos', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero_telefono', 20);
+            $table->unsignedBigInteger('id_contacto');
+            $table->foreign('id_contacto')->references('id')->on('contactos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefonos');
+    }
+}
