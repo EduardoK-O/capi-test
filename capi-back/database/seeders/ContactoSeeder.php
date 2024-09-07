c0195d9701ffa144e4a47ca49430cde4e9c66234
EduardoK-O <e.kuojeda@gmail.com>
2024-09-06 23:11:01 -0600

    Creacción del Seeder del modelo Contacto

--- capi-back/database/seeders/ContactoSeeder.php
+++ capi-back/database/seeders/ContactoSeeder.php
@@ -3,16 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Contacto;
 
-class DatabaseSeeder extends Seeder
+class ContactoSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ContactoSeeder::class);
+        Contacto::factory()->count(500)->create();
     }
 }
